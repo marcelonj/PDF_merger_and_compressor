7d0d7f6ec5375f71ebb91f5c14fc9a6246cf2a42
Marcelo Jurado <marcelonjurado@gmail.com>
2025-03-07 22:28:45 -0300

    Primer commit

---
+++ pdf.py
@@ -0,0 +1,28 @@
+from PyPDF2 import PdfWriter, PdfReader
+from pathlib import Path
+from pikepdf import Pdf
+import os
+
+#Constantes y variables
+DIRECTORIO_ACTUAL = Path(__file__).resolve().parent
+DIRECTORIO_PDF = DIRECTORIO_ACTUAL.joinpath('PDFs')
+PDFs = os.listdir(DIRECTORIO_PDF)
+
+#Se instancia el pdf principal
+combinado = PdfWriter()
+
+#Se agregan los pdf como páginas
+for pdf in PDFs:
+    pagina = PdfReader(DIRECTORIO_PDF.joinpath(pdf))
+    combinado.append(pagina)
+
+#Se guarda el pdf sin comprimir
+salida = open('salida.pdf', 'wb')
+combinado.write(salida)
+combinado.close()
+salida.close()
+
+#Se comprime el pdf resultante y se guarda
+pdf = Pdf.open('salida.pdf')
+pdf.save('salida_comprimida.pdf', compress_streams=True)
+pdf.close()
